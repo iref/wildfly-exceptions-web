373f9ac22728f5b986e4f8f4e1e0510be23bcf44
Jan Ferko <julyloov@gmail.com>
2014-05-20 16:19:45 +0200

    Added arquillian support

--- src/test/java/cz/muni/exceptions/pages/ExceptionsPageTest.java
+++ src/test/java/cz/muni/exceptions/pages/ExceptionsPageTest.java
@@ -1,20 +1,18 @@
 package cz.muni.exceptions.pages;
 
-import cz.muni.exceptions.WicketApplication;
-import cz.muni.exceptions.pages.ExceptionsPage;
-import org.apache.wicket.util.tester.WicketTester;
-import org.junit.Before;
+import cz.muni.exceptions.AbstractExceptionsTest;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
 import org.junit.Test;
 
 /**
  * Simple test using the WicketTester
  */
-public class TestExceptionsPage {
-    private WicketTester tester;
-
-    @Before
-    public void setUp() {
-        tester = new WicketTester(new WicketApplication());
+public class TestExceptionsPage extends AbstractExceptionsTest {    
+    
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return createDeployment();
     }
 
     @Test
